955759aa47873618f79427004a21cdc7331a4ba3
HappyTommy <2522398080@qq.com>
2025-04-10 22:24:49 +0800

    hdssg

---
+++ image.h
@@ -0,0 +1,21 @@
+#ifdef __IMAGE_H
+#define __IMAGE_H
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "bsp_ili9341_lcd.h"
+#include "ff.h" // Include the header file for f_size
+
+
+//=======================================================================================
+//声名函数
+void LCD_Color_DrawPoint(uint16_t x, uint16_t y, uint16_t color);
+void LCD_Draw_Picture_Pro(uint16_t *xstr,uint16_t *ystr,uint16_t sendbyte,uint8_t * pic);
+void open_file_director(char filepath[256000]);
+void LoadAndDisplayCFile(uint16_t *xstr,uint16_t *ystr,char filepath[256000]);
+//=======================================================================================
+//函数声明结束
+
+#endif /* __IMAGE_H */
